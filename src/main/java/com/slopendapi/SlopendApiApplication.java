b778fbfbd033e7945f94130fddf185c86152f103
Tinko Vialard <t.vialard@student.tudelft.nl>
2022-09-28 14:43:20 +0200

    first commit

---
+++ src/main/java/com/slopendapi/SlopendApiApplication.java
@@ -0,0 +1,29 @@
+package com.slopendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SlopendApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.slopendapi.SlopendApiApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/help")
+    public String help() {
+        return "This is a help page";
+    }
+
+}
+
+
