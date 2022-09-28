b15b66bf672b938548f9310cb5404a2c39b7179a
Tinko Vialard <t.vialard@student.tudelft.nl>
2022-09-28 15:10:33 +0200

    update

--- src/main/java/com/slopendapi/SlopendApiApplication.java
+++ src/main/java/com/slopendapi/SlopendApiApplication.java
@@ -3,8 +3,11 @@ package com.slopendapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @SpringBootApplication
 @RestController
@@ -22,6 +25,25 @@ public class SlopendApiApplication {
     @GetMapping("/help")
     public String help() {
         return "This is a help page";
+    }
+
+    @GetMapping("/question")
+    public String question() {
+        return "This is a question page";
+    }
+
+    @GetMapping("/answer")
+    public String answerNoPath() {
+        return "Please respond with a simple /yes or /no appended";
+    }
+
+    @GetMapping("/answer/{answer}")
+    public String answer(@PathVariable String answer) {
+        if (answer.equals("yes")) {
+            return "Glad to hear it, we're gonna make it an amazing year!";
+        } else {
+            return "This is not the answer to the ultimate question of life, the universe, and everything";
+        }
     }
 
 }
